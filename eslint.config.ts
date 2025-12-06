ac92bac3bddd0341dbbd1643663e0dea426dad8c
nanishi2001 <nanishi2001@gmail.com>
2025-12-07 02:46:18 +0900

    refactor: organize ESLint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,41 @@
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
+import { configs } from 'typescript-eslint';
+import unicorn from 'eslint-plugin-unicorn';
+import importPlugin from 'eslint-plugin-import';
 import playwright from 'eslint-plugin-playwright';
 import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: [
-      '.vscode/',
-      '.yarn/',
-      'node_modules/',
-      'yarn.lock',
-      'playwright-report/',
-      'test-results/',
-    ],
+    ignores: ['.vscode/', 'node_modules/', 'playwright-report/', 'test-results/'],
   },
-  ...tseslint.configs.recommended,
-  playwright.configs['flat/recommended'],
+  {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+  },
+  js.configs.recommended,
+  ...configs.recommended,
+  unicorn.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  {
+    files: ['**/*.{ts,js}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  },
+  {
+    ...playwright.configs['flat/recommended'],
+    files: ['tests/**/*.spec.ts'],
+  },
   prettierConfig,
 ];
