efed166bc20cbc6a03ab5dea40fec7e70192d7f3
nanishi2001 <nanishi2001@gmail.com>
2026-01-03 16:19:30 +0900

    feat: implement signup page object model with form filling functions and initial UI tests

--- pages/types.ts
+++ pages/types.ts
@@ -8,3 +8,16 @@ export type PlanName =
   | 'テーマパーク優待プラン';
 
 export type PlanIdMap = Record<PlanName, number>;
+
+export type SignupFormValues = {
+  readonly email?: string;
+  readonly password?: string;
+  readonly passwordConfirmation?: string;
+  readonly username?: string;
+  readonly rank?: 'premium' | 'normal';
+  readonly address?: string;
+  readonly tel?: string;
+  readonly gender?: '男性' | '女性' | 'その他' | '未回答';
+  readonly birthday?: string; // YYYY-MM-DD
+  readonly notification?: boolean;
+};
